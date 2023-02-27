11c00f9d68bcf817b2f27fd8c29dd00eaec2d417
Mohamed_Anwar <alhamed202024@gmail.com>
2023-02-27 17:38:36 +0300

    hooks 2 object

--- my_app/src/App.js
+++ my_app/src/App.js
@@ -1,22 +1,22 @@
-import {react, useState} from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {React, useState} from 'react'
 
-const  App = () => {
-  const [count, setCount] = useState(0);
-  const [isRed, setRed] = useState(false);
-  const handleIncrement = () => {
-    setCount(count + 1);
-    setRed(!isRed);
-  };
+const App = () => {
+  const [name, setName] = useState({
+    firstName: 'mohamed',
+    lastName: 'anwar'
+  });
   return (
-    <div className="app">
-      <button className="btn" onClick={handleIncrement}>Increment</button>
-      <h1>{count}</h1>
-      <h1 className={isRed ? "red" : ""}>change my color</h1>
-
+    <div>
+      <input type="text" name="fname" value={name.firstName}
+      onChange={e => setName({...name, firstName: e.target.value})}
+      />
+      <input type="text" name="lname" value={name.lastName}
+      onChange={e => setName({...name, lastName: e.target.value})}
+      />
+      <h1>My firstName is {name.firstName}</h1>
+      <h1>My lastName is {name.lastName}</h1>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
