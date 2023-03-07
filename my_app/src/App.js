3683d9d6967def74b2577c5ffafaa4adde1403f6
Mohamed_Anwar <alhamed202024@gmail.com>
2023-03-07 22:58:52 +0300

    Effect 1

--- my_app/src/App.js
+++ my_app/src/App.js
@@ -1,24 +1,13 @@
-import {React, useState} from 'react'
+import {React, useState, useEffect} from 'react'
 
 const App = () => {
-  const [items, setItem] = useState([]);
-  const addItem = () => {
-    setItem([...items, 
-      {
-      id: items.length,
-      value: Math.floor(Math.random() * 10) + 1
-    }
-  ]);
-  };
+  const [count, setCount] = useState(0);
+  useEffect(() => {
+      console.log("useEffect called")
+  });
   return (
     <div>
-     <button onClick={addItem}>Add a Number</button>
-     <ul>
-    
-      {items.map((item) => (
-        <li key={item.id}>{item.value}</li>
-      ))}
-      </ul>
+     App
      </div>
   )
 }
