4fa564de8c1a8137a6487eb465da79230a5650f7
Mohamed Anwar <76563640+MohamedAnwarAlhamed@users.noreply.github.com>
2023-03-14 21:08:28 +0300

    Reducer 4

--- my_app/src/App.js
+++ my_app/src/App.js
@@ -1,34 +1,52 @@
 import { React, useReducer } from 'react'
 // import A from './components/comA'
 
-const initialState = {firstCount:0}
+const initialState = { firstCount: 0, secondCount: 10 }
 const reducer = (state, action) => {
-  switch (action) {
+  switch (action.type) {
     case 'increment':
-      return {firstCount:state.firstCount + 1}
+      return { ...state,firstCount: state.firstCount + action.value }
       break
     case 'decrement':
-      return {firstCount:state.firstCount - 1}
+      return { ...state,firstCount: state.firstCount - action.value }
+      break
+    case 'incrementS':
+      return { ...state,secondCount: state.secondCount + action.value }
+      break
+    case 'decrementS':
+      return { ...state,secondCount: state.secondCount - action.value }
       break
     case 'reset':
       return initialState
       break
     default:
-      return state.firstCount
+      return state
   }
 }
 
 const App = () => {
   const [count1, dispatch1] = useReducer(reducer, initialState)
-  const [count2, dispatch2] = useReducer(reducer, initialState)
 
   return (
     <div>
       <h1> count1 - {count1.firstCount} </h1>
-      <button onClick={() => dispatch1('increment')}>+</button>
-      <button onClick={() => dispatch1('decrement' )}>-</button>
-      <button onClick={() => dispatch1('reset')}>0</button>
+      <button onClick={() => dispatch1({ type: 'increment', value: 5 })}>
+        +
+      </button>
+      <button onClick={() => dispatch1({ type: 'decrement', value: 5 })}>
+        -
+      </button>
+      <button onClick={() => dispatch1({ type: 'reset' })}>0</button>
 
+      <h1> count1 - {count1.secondCount} </h1>
+      <button onClick={() => dispatch1({ type: 'incrementS', value: 5 })}>
+        +
+      </button>
+      <button onClick={() => dispatch1({ type: 'decrementS', value: 5 })}>
+        -
+      </button>
+      <h1>reset all</h1>
+      <button onClick={() => dispatch1({ type: 'reset' })}>0</button>
     </div>
   )
 }
