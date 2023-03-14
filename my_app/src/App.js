fde7556b788b898dce92a4ae4749eab899fe1f16
Mohamed Anwar <76563640+MohamedAnwarAlhamed@users.noreply.github.com>
2023-03-14 20:24:29 +0300

    Context 1

--- my_app/src/App.js
+++ my_app/src/App.js
@@ -1,26 +1,15 @@
-import { React, useState, useEffect } from 'react'
-import axios from 'axios'
+import { React, createContext } from 'react'
+import A from './components/comA'
+
+export const UserContext = createContext()
+export const ChannelContext = createContext()
 
 const App = () => {
-  const [user, setUsers] = useState({})
-  const [id, setId] = useState(1)
-
-  useEffect(() => {
-    //axios.get("https://jsonplaceholder.typicode.com/users")
-    fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
-      .then((response) => {
-        return response.json()
-      })
-      .then((data) => {
-        setUsers(data)
-      })
-  }, [id])
   return (
     <div>
-      <input type="text" value={id} onChange={(e) => setId(e.target.value)} />
-      <ul>
-        <li key={user.id}>{user.name}</li>
-      </ul>
+      <UserContext.Provider value={"mohamed"}>
+        <A />
+      </UserContext.Provider>
     </div>
   )
 }
